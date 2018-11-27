b16586944065c4fe41438564915229ea68662912
Patrick Scott <patrickscott2013@gmail.com>
2018-11-27 15:06:05 -0500

    Getting started

---
+++ Makefile
@@ -0,0 +1,4 @@
+app : main.cpp
+	g++ main.cpp -std=c++11 -Wall
+clean:
+	rm a.out
--- main.cpp
+++ main.cpp
@@ -1,4 +1,34 @@
-/*
- * This is a test .cpp file
- *
- */ 
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+#include <string.h>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+   	ifstream inFile;
+	ofstream outFile;
+	char word;
+	string fileText ="";
+     	string toLookFor=argv[1];
+     	//Open file
+  	inFile.open(argv[1]);
+     	if (!inFile){
+        	cerr << "Unable to open file";
+        	exit(1);
+      	}
+    	char buffer[256];
+      	vector<string> array;
+	while (!inFile.eof())
+      	{
+		inFile.getline(buffer, 256);
+        	fileText = buffer;
+         	array.push_back(fileText);
+      	}
+      	cout << array[0] << endl;
+      	inFile.close();
+}
