a3fc9501564f512f64c846fca5b4386275d37cce
Patrick Scott <patrickscott2013@gmail.com>
2018-11-27 19:11:54 -0500

    can now read file via mmfio

--- mmio.cpp
+++ mmio.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <sys/mman.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
-	cout << "Savage" << endl;
+	int fd = open(argv[1], O_RDONLY);
+	struct stat sb;
+
+	if(fstat(fd, &sb) == -1)
+		perror("couldn't get file size");
+
+	char *file_in_memory = (char*)mmap(NULL, sb.st_size, PROT_READ,MAP_SHARED,fd,0);
+	cout << file_in_memory << endl;
+	close(fd);
 	return 0;
 }
